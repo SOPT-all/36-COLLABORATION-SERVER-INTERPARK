481af100e5a0468155a2ea4b96804bee7357abd2
woals2840 <woals2840@gmail.com>
2025-05-21 14:44:38 +0900

    [fix/#50] Patch 추가

--- src/main/java/com/interpark/global/config/WebConfig.java
+++ src/main/java/com/interpark/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("http://localhost:5173") // 프론트 개발 주소 명시
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
 			.allowCredentials(true); // credentials 허용
 	}
 
